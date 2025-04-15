344503cc39fe683787263f1468e988f8aca76642
Rengark <fjunwei178@gmail.com>
2025-04-15 16:39:49 +0800

    Update configuration files

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ require("monokai").setup({
   underline = true,
   bold = true,
   italic = {
-    strings = true,
+    strings = false,
     comments = true,
     operators = false,
     folds = true,
@@ -20,7 +20,7 @@ require("monokai").setup({
   invert_tabline = false,
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
+  contrast = "",  -- can be "hard", "soft" or empty string
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
