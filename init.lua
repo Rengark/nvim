047bb68948bd41f05e847f918bb28c46d570475c
Rengark <fjunwei178@gmail.com>
2025-04-15 15:25:50 +0800

    Initial commit

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- monokai setup
+-- setup must be called before loading the colorscheme
+-- Default options:
+require("monokai").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = true,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+})
+vim.cmd("colorscheme monokai")
+vim.o.background = "dark"
+vim.cmd([[colorscheme monokai]])
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  { "nxstynate/monokai.nvim", priority = 1000 },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai",
+    },
+  },
+}
